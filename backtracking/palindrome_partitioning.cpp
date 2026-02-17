729b8cf7315d376d19537edab44a9c572e83121f
coustea <3329368499@qq.com>
2026-02-17 10:46:46 +0800

    重构：移除 main 函数并优化回文判断
    - 移除测试代码，只保留 Solution 类
    - 使用双指针法实现 isPalindrome，避免依赖 algorithm 库
    - 符合 LeetCode 提交格式

--- backtracking/palindrome_partitioning.cpp
+++ backtracking/palindrome_partitioning.cpp
@@ -7,20 +7,24 @@
  * 空间复杂度：O(n)
  */
 
-#include <iostream>
-#include <algorithm>
+#include <string>
 #include <vector>
 
 using namespace std;
-
-bool is_palindrome(const std::string& str) {
-    return std::equal(str.begin(), str.begin() + str.size() / 2, str.rbegin());
-}
 
 class Solution {
 private:
     vector<vector<string>> result;
     vector<string> path;
+
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
 
     void backtracking(const string& s, int start) {
         if (start == s.size()) {
@@ -29,9 +33,8 @@ private:
         }
 
         for (int i = start; i < s.size(); i++) {
-            string sub = s.substr(start, i - start + 1);
-            if (is_palindrome(sub)) {
-                path.push_back(sub);
+            if (isPalindrome(s, start, i)) {
+                path.push_back(s.substr(start, i - start + 1));
                 backtracking(s, i + 1);
                 path.pop_back();
             }
@@ -46,25 +49,3 @@ public:
         return result;
     }
 };
-
-int main(){
-    Solution sol;
-
-    vector<string> test_cases = {"aab", "a", "aaa", "ab"};
-
-    for (const auto& s : test_cases) {
-        cout << "字符串 \"" << s << "\" 的回文分割方案：\n";
-        vector<vector<string>> ans = sol.partition(s);
-        for (const auto& scheme : ans) {
-            cout << "  [";
-            for (size_t i = 0; i < scheme.size(); i++) {
-                cout << "\"" << scheme[i] << "\"";
-                if (i < scheme.size() - 1) cout << ", ";
-            }
-            cout << "]\n";
-        }
-        cout << "共 " << ans.size() << " 种方案\n\n";
-    }
-
-    return 0;
-}
